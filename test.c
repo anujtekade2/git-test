65de70a4aaeb73d615747f63909814f37abc83fd
anujtekade2 <anujtekade2@gmail.com>
2026-08-15 12:52:09 +0530

    Initial Commit

---
+++ test.c
@@ -0,0 +1,13 @@
+// Made by Pranav Talmale for testing TDM GCC Compiler
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
